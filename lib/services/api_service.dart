b38fa4a289d9d72a14186eec3fa7d6b8a92b3d95
RegineMegg <regine.mata.20@usj.edu.ph>
2022-07-12 14:22:12 +0800

    Integrate product detail features

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
-
+import '../models/cart_update.dart';
 import '../models/product.dart';
 
 
@@ -19,6 +18,32 @@ class ApiService {
         }
       }
       return products;
+    }).catchError((err) => print(err));
+  }
+
+  Future<Product?> getProduct(int id) {
+    return http.get(Uri.parse('$baseUrl/products/$id')).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return Product.fromJson(jsonData);
+      }
+      return null;
+    }).catchError((err) => print(err));
+  }
+
+  Future<void> updateCart(int cartId, int productId) {
+    final cartUpdate =
+        CartUpdate(userId: cartId, date: DateTime.now(), products: [
+      {'productId': productId, 'quantity': 1}
+    ]);
+    return http
+        .put(Uri.parse('$baseUrl/carts/$cartId'),
+            body: json.encode(cartUpdate.toJson()))
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        print(jsonData);
+      }
     }).catchError((err) => print(err));
   }
 }
